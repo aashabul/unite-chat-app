c30f12fa748ccaa724d2793a598361a77a8271a2
aashabul <aashabulimam@gmail.com>
2022-01-17 14:10:07 +0600

    conversation working

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,19 @@ import Login from "./Login";
 import useLocalStorage from "../hooks/useLocalStorage";
 import DashBoard from "./DashBoard";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ContactsProvider } from "../context/ContactsProvider";
+import { ConversationsProvider } from "../context/ConversationsProvider";
 
 function App() {
   const [id, setId] = useLocalStorage("id");
-  return id ? <DashBoard id={id} /> : <Login onIdSubmit={setId} />;
+  const dashboard = (
+    <ContactsProvider>
+      <ConversationsProvider>
+        <DashBoard id={id} />
+      </ConversationsProvider>
+    </ContactsProvider>
+  );
+  return id ? dashboard : <Login onIdSubmit={setId} />;
 }
 
 export default App;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { useState } from "react";
-import { Nav, Tab } from "react-bootstrap";
+import { Button, Modal, Nav, Tab } from "react-bootstrap";
 import Contacts from "./Contacts";
 
 import Conversations from "./Conversations";
+import NewContactModal from "./NewContactModal";
+import NewConversationModal from "./NewConversationModal";
 
 const CONVERSATIONS_KEY = "conversations";
 const CONTACTS_KEY = "contacts";
 
 export default function SideBar({ id }) {
   const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY);
+  const [modalOpen, setModalOpen] = useState(false);
+  const conversationsOpen = activeKey === CONVERSATIONS_KEY;
+
+  function closeModal() {
+    setModalOpen(false);
+  }
+
   return (
     <div style={{ width: "250px" }} className="d-flex flex-column">
       <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
@@ -29,7 +38,20 @@ export default function SideBar({ id }) {
             <Contacts />
           </Tab.Pane>
         </Tab.Content>
+        <div className="p-2 border-top border-end small">
+          Your Id: <span className="text-muted">{id}</span>
+        </div>
+        <Button onClick={() => setModalOpen(true)} className="rounded-0">
+          New {conversationsOpen ? "Conversation" : "Contact"}
+        </Button>
       </Tab.Container>
+      <Modal show={modalOpen} onHide={closeModal}>
+        {conversationsOpen ? (
+          <NewConversationModal closeModal={closeModal} />
+        ) : (
+          <NewContactModal closeModal={closeModal} />
+        )}
+      </Modal>
     </div>
   );
 }
