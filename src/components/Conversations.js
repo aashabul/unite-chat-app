81c96ff94602522d583b337dd05cc88ab3167ffc
aashabul <aashabulimam@gmail.com>
2022-01-17 18:27:24 +0600

    conversation select added

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -3,12 +3,17 @@ import { ListGroup } from "react-bootstrap";
 import { useConversations } from "../context/ConversationsProvider";
 
 export default function Conversations() {
-  const { conversations } = useConversations();
+  const { conversations, selectConversationIndex } = useConversations();
 
   return (
     <ListGroup variant="flush">
       {conversations.map((conversation, index) => (
-        <ListGroup.Item key={index}>
+        <ListGroup.Item
+          key={index}
+          action
+          active={conversation.selected}
+          onClick={() => selectConversationIndex(index)}
+        >
           {conversation.recipients
             .map((recipient) => recipient.name)
             .join(", ")}
--- src/context/ConversationsProvider.js
+++ src/context/ConversationsProvider.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { useContext } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
 import { useContacts } from "./ContactsProvider";
@@ -15,6 +16,8 @@ export function ConversationsProvider({ children }) {
     []
   );
 
+  const [selectedConversationIndex, setSelectedConversationIndex] = useState(0);
+
   const { contacts } = useContacts();
 
   function createConversation(recipients) {
@@ -23,7 +26,7 @@ export function ConversationsProvider({ children }) {
     });
   }
 
-  const formattedConversations = conversations.map((conversation) => {
+  const formattedConversations = conversations.map((conversation, index) => {
     const recipients = conversation.recipients.map((recipient) => {
       const contact = contacts.find((contact) => {
         return contact.id === recipient;
@@ -31,11 +34,13 @@ export function ConversationsProvider({ children }) {
       const name = (contact && contact.name) || recipient;
       return { id: recipient, name };
     });
-    return { ...conversation, recipients };
+    const selected = index === selectedConversationIndex;
+    return { ...conversation, recipients, selected };
   });
 
   const value = {
     conversations: formattedConversations,
+    selectConversationIndex: setSelectedConversationIndex,
     createConversation,
   };
 
